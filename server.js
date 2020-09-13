7f727c9716054950ebd3fab4f47f7f9e2026f52a
Antonio Guedes <gguedesantonio@gmail.com>
2020-09-13 13:54:49 +1000

    html routes working fix

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
  });
 
 //  routes
- app.use(require("./routes/apiRoutes.js"));
- app.use(require("./routes/htmlRoutes.js"));
+//  app.use(require("./routes/apiRoutes.js"));
+ app.use(require("./routes/html-routes.js"));
  
 
 
