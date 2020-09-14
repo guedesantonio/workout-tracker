b536b2d6678b8ebecf3ef0b8bae7994de1209141
Antonio Guedes <gguedesantonio@gmail.com>
2020-09-14 15:19:55 +1000

    Route post created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,32 +3,40 @@ const router = require("express").Router();
 
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-      .then((workouts) => {
-        res.json(workouts);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
-  
-  //*"/api/workouts"
+        .then((workouts) => {
+            res.json(workouts);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+        .then((workout) => {
+            res.json(workout);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
 
 router.put("/api/workouts/:id", (req, res) => {
-db.Workout.find({})
-    .then((workouts) => {
-    res.json(workouts);
-    })
-    .catch((err) => {
-    console.log(err);
-    });
+    db.Workout.findOneAndUpdate(
+            req.params.id,
+            { $push: { exercises: req.body } },
+            { new: true }
+        )
+        .then((workout) => {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
-  
-//*/api/workouts/range
 
-//* exercise/:?
 
-//* exercise
 
-//* stats
 
 module.exports = router;
